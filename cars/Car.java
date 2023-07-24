62205c5f16cfde973e4fee3792b091489748d663
Martin Blazek <martinblazek6@gmail.com>
2023-07-24 14:20:14 +0200

    geometrics

---
+++ cars/Car.java
@@ -0,0 +1,52 @@
+package cars;
+
+import lombok.Data;
+import java.util.Date;
+public class Car {
+
+    private String name;
+    private String model;
+    private Double price;
+    private Date yearOfManufacture;
+    private EngineType engineType;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Date getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public void setYearOfManufacture(Date yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public EngineType getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(EngineType engineType) {
+        this.engineType = engineType;
+    }
+}
---
+++ cars/CarService.java
@@ -0,0 +1,72 @@
+package cars;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarService {
+    private List<Car> cars;
+
+    public CarService() {
+        cars = new ArrayList<>();
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public void removeCar(Car car) {
+        cars.remove(car);
+    }
+
+    public List<Car> getAllCars() {
+        return new ArrayList<>(cars);
+    }
+
+    public List<Car> getCarsWithV12Engine() {
+        return cars.stream()
+                .filter(car -> car.getEngineType() == EngineType.V12)
+                .collect(Collectors.toList());
+    }
+
+
+    public Car getMostExpensiveCar() {
+        return cars.stream()
+                .max(Comparator.comparingDouble(Car::getPrice))
+                .orElse(null);
+    }
+
+    public Car getCheapestCar() {
+        return cars.stream()
+                .min(Comparator.comparingDouble(Car::getPrice))
+                .orElse(null);
+    }
+
+
+    public List<Car> getCarsSortedByPrice(boolean ascending) {
+        Comparator<Car> comparator = Comparator.comparingDouble(Car::getPrice);
+        if (!ascending) {
+            comparator = comparator.reversed();
+        }
+        return cars.stream().sorted(comparator).collect(Collectors.toList());
+    }
+
+    public boolean containsCar(Car car) {
+        return cars.contains(car);
+    }
+
+
+
+    private boolean evaluateYearComparison(int carYear, String operator, int year) {
+        return switch (operator) {
+            case "<" -> carYear < year;
+            case "<=" -> carYear <= year;
+            case ">" -> carYear > year;
+            case ">=" -> carYear >= year;
+            case "=" -> carYear == year;
+            case "!=" -> carYear != year;
+            default -> false;
+        };
+    }
+}
